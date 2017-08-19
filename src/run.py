8feddc45d0bef7647fc42277ced192405292363c
gvpm <gvillar@id.uff.br>
2017-08-19 12:31:41 -0300

    Added Docker-Compose

--- src/run.py
+++ src/run.py
@@ -9,7 +9,8 @@ from routes.call import create_call
 app = Sanic()
 app.add_route(create_call, '/call', methods=['POST'])
 app.add_route(health_check, '/health-check')
-mongoengine.connect('blah')
+mongoengine.connect('blah', host=os.getenv('MONGO_URL'))
+print('PORRA VAI TOMAR NO CU')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=os.getenv('PORT', 5000))
