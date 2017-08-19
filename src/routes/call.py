1a8fc472ef07c2edda19161d47a12b0825f3b1b4
Bianca Rosa <me@biancarosa.com.br>
2017-08-19 05:22:05 -0300

    Dont break with empty JSON body

--- src/routes/call.py
+++ src/routes/call.py
@@ -4,10 +4,9 @@ from services.call import add_call
 import mongoengine
 
 async def create_call(request):
-    call_details = request.json
     return_data = {"messages" : [], "success": False, "data": {}}
-
-    return_data['data'] = add_call(call_details['file_uri'], call_details['metadata'])
-    return_data['success'] = True
-    print(return_data)
+    if request.json:
+        call_details = request.json
+        return_data['data'] = add_call(call_details['file_uri'], call_details['metadata'])
+        return_data['success'] = True
     return json(return_data)
