e21017fda6af92a54fe93af476cbd8a6974f5a1d
Bianca Rosa <me@biancarosa.com.br>
2017-08-19 05:23:52 -0300

    Return bad request status code

--- src/routes/call.py
+++ src/routes/call.py
@@ -5,8 +5,9 @@ import mongoengine
 
 async def create_call(request):
     return_data = {"messages" : [], "success": False, "data": {}}
-    if request.json:
-        call_details = request.json
-        return_data['data'] = add_call(call_details['file_uri'], call_details['metadata'])
-        return_data['success'] = True
+    if not request.json:
+        return json(return_data, status=400)
+    call_details = request.json
+    return_data['data'] = add_call(call_details['file_uri'], call_details['metadata'])
+    return_data['success'] = True
     return json(return_data)
