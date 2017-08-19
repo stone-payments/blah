4a00737317db1e9be52b3790afefa61e7834e73e
gvpm <gvillar@id.uff.br>
2017-08-19 10:11:19 -0300

    Adding one more analisys

--- src/services/speech_to_sentiment.py
+++ src/services/speech_to_sentiment.py
@@ -12,6 +12,28 @@ def transcribe_file(speech_info):
     annotations = text_to_sentiment.analyze(response)
     sentiment_result = text_to_sentiment.get_result(annotations)
     
+    sentiment_result["simplified"] = get_simplified_analisys(sentiment_result)
+
     return sentiment_result
+
+
+def get_simplified_analisys(sentiment_result):
+    score = sentiment_result["score"]*100
+    magnitude = sentiment_result["magnitude"]*100
+
+    balanced_score = score*magnitude    
+    print(balanced_score)
+
+    if (balanced_score >= -15 and balanced_score < 15):
+        return "neutral"
+    elif(balanced_score >= 15 and balanced_score < 40):
+        return "positive"
+    elif(balanced_score >= 40):
+        return "very positive"
+    elif (balanced_score >= -40 and balanced_score < -15):
+        return "negative"
+    elif (balanced_score < -40):
+        return "very negative"
+        
 
 
