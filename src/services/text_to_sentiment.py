6765d5ea99772418f24297e8e5f046285a48eb4f
Bianca Rosa <me@biancarosa.com.br>
2017-08-19 11:51:32 -0300

    clean file

--- src/services/text_to_sentiment.py
+++ src/services/text_to_sentiment.py
@@ -1,26 +1,19 @@
-# [START import_libraries]
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-# [END import_libraries]
-# [START def_analyze]
-def analyze(textToAnalyze):
-	"""Run a sentiment analysis request on text within a passed filename."""
+
+def analyze(text_to_analyze):
 	client = language.LanguageServiceClient()
 	document = types.Document(
-		content=textToAnalyze,
+		content=text_to_analyze,
 		type=enums.Document.Type.PLAIN_TEXT)
 	annotations = client.analyze_sentiment(document=document)
-	# Print the results    
 	return annotations
-# [END def_analyze]
 
-# [START def_print_result]
 def get_result(annotations):
 	score = annotations.document_sentiment.score
 	magnitude = annotations.document_sentiment.magnitude
 	dict = {'score': score, 'magnitude': magnitude}
 	return dict
-# [END def_print_result]
 
 
