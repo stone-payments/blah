cc07f682d87fb6b6789e228fdde9c6cdcb280177
Allan J <contato.allanj@gmail.com>
2017-08-20 03:34:49 -0300

    update

--- src/routes/twilio.py
+++ src/routes/twilio.py
@@ -3,13 +3,14 @@ from twilio.rest import Client
 from sanic.response import text, html
 
 async def rec_message(request):
-    print(str(request))
     response = VoiceResponse()
     response.say('Recording...', voice='woman')
-    response.record(recording_status_callback='/twilio-record')
+    # response.record(recording_status_callback='/twilio-record')
+    response.record(recording_status_callback='https://requestb.in/12vdrvo1')
     return html(str(response))
 
 async def twilio_recording(request):
+    print(request)
     recording_url = request.values.get('RecordingUrl', None)
     print(recording_url)
     return text('ok')
